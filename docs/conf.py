9529202cd7ed9bc85341200273d12f3624955b5f
Mark Edmonds <mark@mjedmonds.com>
2020-06-02 10:37:23 -0400

    attempt 7 - update RTD theme...browser cache...

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'doxygen-test'
+project = 'doxygen-test-cpp-python'
 copyright = '2020, Mark Edmonds'
 author = 'Mark Edmonds'
 
@@ -98,17 +98,6 @@ autoapi_root = "py_api"
 # `sphinx` works very well with the RTD theme, but you can always change it
 pygments_style = 'sphinx'
 
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# [[[ end theme marker ]]]
-
-# NOTE: this is only here for me to auto-place sections of conf.py in the docs
-#       but isn't needed in producion releases
 html_theme = 'sphinx_rtd_theme'
 
 html_title = "Doxygen test: merging C++ and Python API references"
